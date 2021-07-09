6b009c3f09f881db26082f12f6677da1dfe8932b
hailanderhb <hailanderhb@gmail.com>
2021-07-09 14:35:21 -0300

    Algoritmo para calcular média de idades e/ou numeros inteiros

---
+++ mediaAlunos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace mediaAlunos
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int soma = 0;
+            float media;
+
+
+            for (int aluno = 0; aluno < 5; aluno++)
+            {
+                Console.WriteLine("Digite a idade do primeiro aluno: ");
+                int idade = int.Parse(Console.ReadLine());
+                soma += idade;
+            }
+
+            media = soma / 5;
+            Console.WriteLine($"A média dos 5 alunos é de {media}");
+            Console.ReadLine();
+        }
+    }
+}
