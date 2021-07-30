f2a717db00374a4ac267d5de1a4c5f39b2d3d02b
HailanderJs <38103099+hailanderhb@users.noreply.github.com>
2021-07-30 09:25:19 -0300

    Utilização de dapper ou invés de Ado.net
    Utilização de dapper ou invés de Ado.net, visando rendimento.
    
    Utilização de: Listar categorias, Criar categorias, Atualizar Categorias, Deletar Categoria, Criar muitas Categorias, Execução de Procedure, Leitura de Procedure, Execução de Scalar (para listar ao mesmo tempo que insere uma nova categoria), Leitura de View.

--- dapper.cs
+++ dapper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using BaltaDataAccess.Model;
 using Dapper;
 using Microsoft.Data.SqlClient;
@@ -40,12 +41,15 @@ namespace BaltaDataAccess
                 // }
                 //----------------------------------------------------------------------
 
-                ListCategories(connection);
+                //ListCategories(connection);
                 //CreateCategory(connection);
                 //UpdateCategory(connection);
                 //DeleteCategory(connection);
-
-
+                //CreateManyCategory(connection);
+                //ExecuteProcedure(connection);
+                //ExecuteReadProcedure(connection);
+                //ExecuteScalar(connection);
+                ReadView(connection);
 
 
             }
@@ -115,9 +119,134 @@ namespace BaltaDataAccess
             var deleteCategory = "DELETE [Category] WHERE [Id]=@id";
             var rows = connection.Execute(deleteCategory, new
             {
-                id = new Guid("4e2925c0-3755-430f-b998-ed1156e60e82")
+                id = new Guid("d11e19e0-745f-417e-8e97-f53e9b370198")
             });
             Console.WriteLine($"{rows} registros excluídos");
+        }
+
+        static void CreateManyCategory(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "Aws Cloud";
+            category.Featured = false;
+
+            var category2 = new Category();
+            category2.Id = Guid.NewGuid();
+            category2.Title = "Categoria Nova";
+            category2.Url = "Categoria Nova";
+            category2.Description = "Categoria Nova";
+            category2.Order = 9;
+            category2.Summary = "Categoria Nova";
+            category2.Featured = true;
+
+            //SQL Injection
+
+            var insertSql = @"INSERT INTO [Category] 
+            VALUES (
+                @Id, 
+                @Title, 
+                @Url, 
+                @Summary, 
+                @Order, 
+                @Description, 
+                @Featured)";
+
+            var rows = connection.Execute(insertSql, new[]{
+                new{
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                },
+                new{
+                    category2.Id,
+                    category2.Title,
+                    category2.Url,
+                    category2.Summary,
+                    category2.Order,
+                    category2.Description,
+                    category2.Featured
+                }
+            });
+
+
+            Console.WriteLine($"{rows} linhas inseridas");
+        }
+
+        static void ExecuteProcedure(SqlConnection connection)
+        {
+            var procedure = "[spDeleteStudent]";
+            var pars = new { StudentId = "7e4e7f71-5444-473b-83a1-aefaf68f7ea9" };
+
+            var affectedRows = connection.Execute(procedure, pars, commandType: System.Data.CommandType.StoredProcedure);
+
+            Console.WriteLine($"{affectedRows} linhas afetadas");
+        }
+        static void ExecuteReadProcedure(SqlConnection connection)
+        {
+            var procedure = "[spGetCoursesByCategory]";
+            var pars = new { CategoryId = "09ce0b7b-cfca-497b-92c0-3290ad9d5142" };
+
+            var courses = connection.Query(procedure, pars, commandType: System.Data.CommandType.StoredProcedure);
+
+            foreach (var item in courses)
+            {
+                Console.WriteLine(item.Id);
+            }
+        }
+
+        static void ExecuteScalar(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "Aws Cloud";
+            category.Featured = false;
+
+            //SQL Injection
+
+            var insertSql = @"INSERT INTO [Category] 
+            OUTPUT inserted.[Id]
+            VALUES (
+                NEWID(), 
+                @Title, 
+                @Url, 
+                @Summary, 
+                @Order, 
+                @Description, 
+                @Featured)";
+
+            var id = connection.ExecuteScalar<Guid>(insertSql, new
+            {
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+
+            Console.WriteLine($"A categoria inserida foi: {id}");
+        }
+
+        static void ReadView(SqlConnection connection)
+        {
+            var sql = "SELECT * FROM [vwCourses]";
+            var courses = connection.Query(sql);
+            foreach (var item in courses)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
         }
     }
 
